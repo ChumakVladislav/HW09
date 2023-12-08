d9991c572a8403d11e77ce2ddafc97c918699b16
ChumakVladislav <chumakvolv@gmail.com>
2023-12-08 22:44:33 +0200

    last commit finally

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+// Import library
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Get form element
+const formRef = document.querySelector('.form');
+
+// Set event listener submit on form
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let step = Number(formRef.delay.value);
+
+  for (let i = 1; i <= formRef.amount.value; i++) {
+    createPromise(i, step);
+    step += Number(formRef.step.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
